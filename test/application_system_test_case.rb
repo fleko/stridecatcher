3701a5dc974658fce21a1dce96748577c59d4e57
Ezequiel Aranda <earanda@gmail.com>
2021-02-09 09:02:34 -0300

    Fix for Travis CI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,8 +2,13 @@ require "test_helper"
 require "socket"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
-    url: ENV.fetch("SELENIUM_REMOTE_URL")
-  }
+  if ENV["SELENIUM_REMOTE_URL"]
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
+      url: ENV.fetch("SELENIUM_REMOTE_URL")
+    }
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  end
+
   include Devise::Test::IntegrationHelpers
 end
